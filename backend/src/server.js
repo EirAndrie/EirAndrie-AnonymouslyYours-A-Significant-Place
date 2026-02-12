e9c879bce33bfa2af73c35aac70e25233c78735f
codeWithHrieu <hrieu01@gmail.com>
2026-02-12 22:04:15 +0800

    Minimal modifications for development and depployment configurations

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,8 +20,17 @@ app.use(
 
 app.use(express.json());
 app.use("/", PlaceRoutes);
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("*");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 connectDB().then(() => {
   console.log("MONGODB connected: SERVER file");
